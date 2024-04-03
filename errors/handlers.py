752abd640465babe979e6f5b4fa9eaef07828e72
Julian Bottero <bottero35@gmail.com>
2024-04-03 18:19:23 -0400

    poop

--- errors/handlers.py
+++ errors/handlers.py
@@ -2,10 +2,10 @@ from flask import render_template, Blueprint
 
 errors = Blueprint('errors', __name__)
 
-@errors.app_errorhandler(404)
+@errors.errorhandler(404)
 def page_not_found(e):
     return render_template("errors/404.html"), 404
 
-@errors.app_errorhandler(505)
+@errors.errorhandler(505)
 def page_not_found(e):
     return render_template("errors/505.html"), 505
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, request
 from utils.database import insert_user, user_in_database, password_check
+from errors.handlers import *
 
 # Load Instance
 app = Flask(__name__)
